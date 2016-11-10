78760a8d17c7fe2d596bce0f0f3dca2f560c421a
pbellon <bellon.pierre@gmail.com>
2016-11-10 18:46:25 +0100

    Create Brand component

---
+++ src/components/Brand/Brand.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+import Link from '../Link';
+import s from './Brand.css';
+
+function Brand(){
+  return (
+    <Link className={s.brand} to="//skoli.fr" rel="nofollow" target="_blank">Skoli</Link>
+  )
+}
+
+export default withStyles(s)(Brand);
