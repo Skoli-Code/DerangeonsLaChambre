66aa1338f7c012d975e1b508dd58bd854ecc8d0c
pbellon <bellon.pierre@gmail.com>
2016-11-15 15:56:41 +0100

    Add safe check for Helmet-set head variable in Html component

--- src/components/Html.js
+++ src/components/Html.js
@@ -11,12 +11,17 @@ import React, { PropTypes } from 'react';
 import { analytics } from '../config';
 
 function Html({ head, style, script, chunk, children }) {
+  let meta, title;
+  if(head){
+    meta = head.meta.toComponent();
+    title = head.title.toComponent();
+  }
   return (
     <html className="no-js" lang="en">
       <head>
         <meta charSet="utf-8" />
-        { head.meta.toComponent() }
-        { head.title.toComponent() }
+        { meta }
+        { title }
 
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
