c467542613dc2e4b89318f238e98c342947bad02
pbellon <bellon.pierre@gmail.com>
2016-12-05 19:22:37 +0100

    Raise font base size

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,16 +11,11 @@ import React, { PropTypes } from 'react';
 import Helmet from 'react-helmet';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Layout.css';
-import Header from '../Header';
 import Footer from '../Footer';
 
 function Layout({ children }) {
   return (
     <div>
-      <Helmet
-        title="Acceuil"
-        titleTemplate="%s | Dérangeons la Chambre"
-      />
       {children}
     </div>
   );
